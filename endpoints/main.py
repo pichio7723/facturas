c8a5ed7f76c0f2e948aa74abfb2b343b1d2b2f42
Brayan Estiven Rojas Rueda <pichirionix7723@gmail.com>
2026-06-15 20:03:12 -0500

    feat: modelo en forma base completo clientes, se añaden dos endpoints nuevos, uno para crear clientes y otro para listarlos, se controla una excepsion si no hay usuarios registrados, no se lista

--- endpoints/main.py
+++ endpoints/main.py
@@ -1,3 +1,37 @@
 from fastapi import FastAPI
+import asyncio
+from models.cliente import clienteBase, clienteCrear, clienteEditar, cliente
+from fastapi import HTTPException
 
 app = FastAPI()
+
+usuarios = []
+ultimo_id = 0
+
+@app.post("/usuarioCrear")
+def crear_usuario(usuario : clienteCrear):
+    global ultimo_id
+    ultimo_id += 1
+    #definicion de cliente
+    cliente_nuevo = cliente(
+        id = ultimo_id,
+        **usuario.model_dump()
+    )
+    usuarios.append(cliente_nuevo)
+
+    return {
+        "mensaje" : "usuario creado exitosamente",
+        "usuario" : cliente_nuevo
+    }
+
+
+@app.get("/usuarios")
+async def listar_todos():
+    if not usuarios:
+        raise HTTPException(
+            status_code=404,
+            detail="No hay usuarios registrados."
+        )
+    return usuarios
+
+
---
+++ models/cliente.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class clienteBase(BaseModel):
+    nombre : str
+    edad : int
+    saldo : int
+
+class clienteCrear(clienteBase):
+    pass
+
+class clienteEditar(clienteBase):
+    nombre: str | None = None
+    edad: int | None = None
+    saldo: int | None = None
+
+class cliente(clienteBase):
+    id : int | None = None
